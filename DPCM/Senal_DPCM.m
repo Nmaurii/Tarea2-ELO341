20da237ab05feda15be2b7c8d12e604a6de367d8
Nmaurii <144194768+Nmaurii@users.noreply.github.com>
2024-10-20 04:53:48 -0300

    Update Senal_DPCM.m

--- DPCM/Senal_DPCM.m
+++ DPCM/Senal_DPCM.m
@@ -6,121 +6,86 @@ clc;
 clear all;
 close all;
 
-%utilizando la misma senal banda base del item PCM    
-function d_q = DPCM(m,orden,a_n)
-    k = orden+1;
-    m_q = zeros(1,length(m));
-    d_q = zeros(1,length(m));
+function [valor_cuantizado, error] = Cuantizar(valor, n_bits, mp)
+
+    L = 2^n_bits; %n umero de niveles
+    delta = 2 * mp / L; % tamano de cada nivel
+    e = delta / 2; % error medio de los delta
+    niveles = -mp + e : delta : mp - e; 
+    dif = abs(valor - niveles); 
+    [~, idx] = min(dif); % encuentra el indice del nivel mas cercano
+    valor_cuantizado = niveles(idx); % valor cuantizado
+    error = abs(valor - valor_cuantizado); % error de cuantización
+end
+
+%Funcion modulacion DPCM
+function d_q = DPCM(m, orden, a_n, n_bits, mp)
+    k = orden + 1;
+    m_q = zeros(1, length(m));
+    d_q = zeros(1, length(m));
 
     for i = k:length(m)
-
-        prediccion = a_n(1)*m_q(i-1) + a_n(2)*m_q(i-2) + a_n(3)*m_q(i-3);
-        d_q(i) = m(i) - prediccion;
-        m_q(i) = d_q(i) - prediccion;
+        prediccion = a_n(1) * m_q(i-1) + a_n(2) * m_q(i-2) + a_n(3) * m_q(i-3);
+        d_q(i) = Cuantizar(m(i) - prediccion, n_bits, mp);
+        m_q(i) = prediccion + d_q(i); % reconstrucción correcta en el transmisor
     end
 end
 
-%
-function [q,error,SNR] = Cuantizar(y,n_bits,mp)
-    
-    L = 2^n_bits; 
-    delta = 2*mp/L;
-    e = delta/2; 
-    niveles = -mp + e : delta : mp - e; 
-
-    for i = 1:length(y)
-        dif = abs(y(i) - niveles); 
-        [minimo, idx] = min(dif);  
-        q(i) = niveles(idx); 
-
-        SNR(i) = 10*log10((y(i)^2)/(mp^2/(3*(n_bits)^2))); 
-        error(i) = abs(y(i) - q(i));
-    end
-end
-
-
-% function [q,error] = Cuantizar(y,n_bits,mp)
-% 
-%     L = 2^n_bits; 
-%     delta = 2*mp/L;
-%     e = delta/2; 
-%     niveles = -mp + e : delta : mp - e; 
-% 
-%     for i = 1:length(y)
-%         dif = abs(y(i) - niveles); 
-%         [minimo, idx] = min(dif);  
-%         q(i) = niveles(idx); 
-% 
-%         error(i) = abs(y(i) - q(i));
-%     end
-% end
-
-
-%demodulacion DPCM 
+% Funcion demodulacion DPCM
 function m = DPCD(d_q, orden, a_n)
     k = orden + 1;
     m = zeros(1, length(d_q));
+    m_q = zeros(1, length(d_q)); 
 
     for i = k:length(d_q)
-        prediccion = a_n(1) * d_q(i-1) + a_n(2) * d_q(i-2) + a_n(3) * d_q(i-3);
-        m(i) = d_q(i) + prediccion; 
+        % predicción usando los coeficientes a_n
+        prediccion = a_n(1) * m_q(i-1) + a_n(2) * m_q(i-2) + a_n(3) * m_q(i-3);
+        m_q(i) = prediccion + d_q(i); % reconstrucción en el receptor
+        m(i) = m_q(i); % senal demodulada
     end
 end
 
 %****** TRANSMISOR
 
 %----------SAMPLEO
-
-f = 1000; % frecuancia de senal banda base 1000 Hz
+f = 1000; % Frecuencia de señal banda base 1000 Hz
 n_muestras = 1000;
-fs = 2*f*n_muestras; % frecuencia de muestreo (Nyquist)
+fs = f * n_muestras; % Frecuencia de muestreo para 1000 muestras por ciclo
 
-T = 1/f; % periodo de un ciclo
-N = (T*fs); %cantidad de muestas
+T = 1 / f; % Periodo de un ciclo
+N = round(T * fs); % Cantidad de muestras
 
-t = linspace(0,T,N);%intervalo de valores.
+t = linspace(0, T, N); % Intervalo de valores.
 
-% senal a modular
-A = 1;%amplitud
-m = A*sin(2*pi*f*t);
-
-%---------MODULACION DPCM
-senalDPCM_plana = DPCM(m,3,[0.7071,0.5,0.25]);
+% Señal a modular
+A = 1; % Amplitud
+m = A * sin(2 * pi * f * t);
 
 %----------CUANTIZACION
-mp = max(senalDPCM_plana); %dado que DPCM contrae la senal, se recalcula el maximo de la senal
-disp(mp);
-cantidad_bits = 4;
+cantidad_bits = 3;
 
-%la senalDPCM_cuantizada corresponde a la TX
-[senalDPCM_cuantizada,error,SNR] = Cuantizar(senalDPCM_plana,cantidad_bits,mp);
+%---------MODULACION DPCM
+senalDPCM_plana = DPCM(m, 3, [0.7071, 0.5, 0.25], cantidad_bits, A);
 
 %****** RECEPTOR
+senal_demodulada = DPCD(senalDPCM_plana, 3, [0.7071, 0.5, 0.25]);
 
-senal_demodulada = DPCD(senalDPCM_cuantizada,3,[0.7071,0.5,0.25]);
+%********Grafica
 
-figure;
-
-plot(t, m, '.-', 'DisplayName', 'Señal Sampleada'); 
+figure(1);
+plot(t, m, 'r--', 'DisplayName','Señal'); 
 hold on;
-plot(t, senalDPCM_plana, '.', 'DisplayName', 'Señal DPCM Plana TX'); 
-plot(t, senalDPCM_cuantizada, 'r--', 'DisplayName', 'Señal DPCM cuantizada TX'); 
+plot(t, senalDPCM_plana, 'b-', 'DisplayName', 'Senal Modulada'); 
+
+plot(t, senal_demodulada, '-', 'DisplayName', 'Señal Demodulada');
 hold off;
 
-xlabel('Tiempo 1[ms]');
-title('Señal Tx');
+
+
+xlabel('tiempo [s]');
+ylabel('Amplitud [V]');
+title('Señal DPCM con profundidad de 3 bits');
 legend('show');
 grid on;
 xlim([0, T]);
-
-figure;
-
-plot(t, senal_demodulada, 'r--', 'DisplayName', 'Señal DPCM demodulada RX'); 
-hold on;
-
-
-xlabel('Tiempo 1[ms]');
-title('Señal Demodulada Rx');
-legend('show');
-grid on;
-xlim([0, T]);
+ylim([-1, 1]); 
